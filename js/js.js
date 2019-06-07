a31a47e7f2c7a5ac3ac6131a2fd40d3b3a843f6a
PatrickSkk <45877606+PatrickSkk@users.noreply.github.com>
2019-06-07 19:44:45 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,6 @@
+var screenheight = (screen.height / 2)+(screen.height / 3)-(screen.height / 11)+(screen.height / 18);
+var heightscreen = screenheight + "px";
+document.getElementById('header').style.height = heightscreen;
+var screenheight2 = (screen.height / 6);
+var heightscreen2 = screenheight2 + "px";
+document.getElementById('header2').style.height = heightscreen2;
